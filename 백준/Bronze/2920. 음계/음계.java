d334e49326ad8e8de9da9ed3a9d27619c74c912e
CODE_NAME_PLATYPUS <uimi3606@gmail.com>
2025-12-22 20:21:59 +0900

    [Bronze II] Title: 음계, Time: 100 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        
+        String input = bf.readLine();
+        
+        if (input.equals("1 2 3 4 5 6 7 8")) {
+            System.out.println("ascending");
+        } else if (input.equals("8 7 6 5 4 3 2 1")) {
+            System.out.println("descending");
+        } else {
+            System.out.println("mixed");
+        }
+    }
+}
