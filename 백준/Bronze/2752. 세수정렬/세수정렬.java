c94b448d87fb2cb8b08131f4e074932f63744b48
CODE_NAME_PLATYPUS <uimi3606@gmail.com>
2025-12-27 16:06:44 +0900

    [Bronze IV] Title: 세수정렬, Time: 136 ms, Memory: 16344 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.java
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -2,17 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
-    public static void main(String[] args)throws IOException{
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
-        
-        int[] numbers = {A, B, C};
-        Arrays.sort(numbers);
-        
-        System.out.println(numbers[0] + " " + numbers[1] + " " + numbers[2]);
-    }
+	public static void main(String[] args)throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		
+		int[] numbers = new int[3];
+		
+		numbers[0] = Integer.parseInt(st.nextToken());		
+		numbers[1] = Integer.parseInt(st.nextToken());	
+		numbers[2] = Integer.parseInt(st.nextToken());	
+		
+		Arrays.sort(numbers);
+		
+		System.out.println(numbers[0] + " " + numbers[1] + " " + numbers[2]);
+	}
 }
