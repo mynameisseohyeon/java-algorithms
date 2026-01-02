5f22c97525f05deff5bffd98ba0801e7c9baf4b7
CODE_NAME_PLATYPUS <uimi3606@gmail.com>
2026-01-02 19:57:23 +0900

    [Bronze II] Title: 숫자의 개수, Time: 100 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -2,27 +2,25 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
-    public static void main(String[] args)throws IOException{
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        
-        int A = Integer.parseInt(bf.readLine());
-        int B = Integer.parseInt(bf.readLine());
-        int C = Integer.parseInt(bf.readLine());
-        
-        int multiResult = A * B * C;
-        
-        String strResult = String.valueOf(multiResult);
-        
-        for(int i = 0; i <= 9; i++) {
-            int count = 0;
-            
-            for (int j = 0; j < strResult.length(); j++) {
-                if((strResult.charAt(j) - '0') == i) {
-                    count++;
-                }
-            }
-            
-            System.out.println(count);
-        }
-    }
+	public static void main(String[] args)throws IOException{ // 숫자의 개수(2577)
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		int C = Integer.parseInt(br.readLine());
+		
+		int multi = A * B * C;
+		String str = String.valueOf(multi);
+		
+		int[] arrNum = new int[10];
+		
+		for(int i = 0; i < str.length(); i++) {
+			int digit = str.charAt(i) - '0'; // 문자 -> 숫자
+			arrNum[digit]++;
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(arrNum[i]);
+		}
+	}
 }
