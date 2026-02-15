209adac4a3629d4abdd68aa90029201123e8df00
CODE_NAME_PLATYPUS <uimi3606@gmail.com>
2026-02-15 20:18:46 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14340 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException { // Hashing
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int L = Integer.parseInt(br.readLine()); // 문자열 길이
+        String s = br.readLine();
+        long M = 1234567891;
+        long result = 0;
+        long power = 1; // 31^i
+
+        for(int i = 0; i < L; i++) {
+            int num = s.charAt(i) - 'a' + 1; // a는 1, b는 2, ... z는 26
+            result += (num * power) % M; // 오버플로우 방지
+            power = power * 31 % M; // 다음 자리수의 가중치 계산
+        }
+        System.out.println(result);
+
+    }
+}
