c2d25a39c06138e9b627771061d2ab0874f81fc8
CODE_NAME_PLATYPUS <uimi3606@gmail.com>
2026-07-15 23:42:49 +0900

    [level 0] Title: 배열 자르기, Time: 0.04 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        
+        return Arrays.copyOfRange(numbers, num1, num2 + 1);
+    }
+}
