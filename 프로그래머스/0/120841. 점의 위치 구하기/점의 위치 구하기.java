f68e4ea18803a50f861e4d783db62046c630f350
CODE_NAME_PLATYPUS <uimi3606@gmail.com>
2026-07-19 23:56:27 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.02 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int[] dot) {
+        int answer = (dot[0] > 0 && dot[1] > 0) ? 1 : (dot[0] > 0 && dot[1] < 0) ? 4 : (dot[0] < 0 && dot[1] < 0) ? 3 : 2;
+        
+        return answer;
+    }
+}
