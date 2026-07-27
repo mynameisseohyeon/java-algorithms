4160f96763fb0876f7b368e07eadd3039335c089
CODE_NAME_PLATYPUS <uimi3606@gmail.com>
2026-07-27 20:50:48 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.47 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder answer = new StringBuilder(); 
+            
+        for(char ch : my_string.toCharArray()) {
+            if(Character.isUpperCase(ch)) {
+                answer.append(Character.toLowerCase(ch));
+            } else if(Character.isLowerCase(ch)) {
+                answer.append(Character.toUpperCase(ch));
+            }
+        }
+        
+        return answer.toString();
+    }
+}
