aaf0bec8f1f97e91e0f8714e0b58b3739abcfc9b
CODE_NAME_PLATYPUS <uimi3606@gmail.com>
2026-07-30 15:34:09 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.04 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(int n, int[] numlist) {
+        List<Integer> answer = new ArrayList<>();
+        
+        for(int i = 0; i < numlist.length; i++) {
+            if(numlist[i] % n == 0) {
+                answer.add(numlist[i]);
+            }
+        }
+            
+        return answer;
+    }
+}
